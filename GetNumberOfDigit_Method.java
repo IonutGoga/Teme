834fd832dd26c640c2b9cc95d38651c18931ee87
IonutGoga <49606175+IonutGoga@users.noreply.github.com>
2019-05-06 09:18:09 -0700

    Delete GetNumberOfDigit_Method.java

--- GetNumberOfDigit_Method.java
+++
@@ -1,9 +0,0 @@
-package Teme.Teme11_May;
-
-public class GetNumberOfDigit_Method {
-
-    public GetNumberOfDigit_Method(Integer b){
-        String c = b.toString();
-        System.out.println(c.length());
-    }
-}
